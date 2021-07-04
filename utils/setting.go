2be8969835b904bc9f67009345315e8e2107a312
HanFengA7 <tancan137@foxmail.com>
2021-07-04 16:26:36 +0800

    update

--- utils/setting.go
+++ utils/setting.go
@@ -1,1 +1,16 @@
 package utils
+
+import (
+	"bytes"
+	"io/ioutil"
+
+	"gopkg.in/ini.v1"
+)
+
+func main() {
+	cfg, err := ini.Load(
+		[]byte("raw data"), // 原始数据
+		"filename",         // 文件路径
+		ioutil.NopCloser(bytes.NewReader([]byte("some other data"))),
+	)
+}
